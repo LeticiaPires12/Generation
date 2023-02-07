08587c0d3ae5c84411a9914a1e6ad4786ca14ade
Leticia <leticiapiress1@outlook.com>
2023-02-07 11:54:09 -0300

    Hello Word

---
+++ helloword/src/helloword/HelloWord.java
@@ -0,0 +1,11 @@
+package helloword;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+	
+      System.out.println("Hello Word!");
+      System.out.println("Turma 61 é top!");
+	}
+
+}
